cc0a8a01d18a80cb7961973ea9110562053e70bb
Hannes Eilers <hanneseilers@gmx.de>
2025-10-28 09:47:51 +0100

    Udates and added uni tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
-// Basic bootstrap for tests: include project files
+// Basic bootstrap for tests: include Composer autoload if available and project files
+$autoloader = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloader)) {
+	require_once $autoloader;
+}
+
+// Include lightweight project bootstrap for tests (Database etc.)
 require_once __DIR__ . '/../storage.php';
